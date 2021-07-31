544ff5b321717a7cba2b4b25375768f1f5f576e9
AlefeOliveira5 <lefeoliveira@gmail.com>
2021-07-31 20:05:56 -0300

    configurações iniciais e primeira index

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,10 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+ mix.js("resources/js/app.js", "public/js")
+ 
+ mix.sass('resources/sass/app.scss', 'public/css')
+   options({
+      processCssUrls: false,
+      postCss: [ tailwindcss('tailwind.config.js')]
+   })
